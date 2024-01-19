9413bca9f7954060260757b78b1c42ec7d2c2efa
Carla Herrera <cgherrera07@gmail.com>
2024-01-19 15:46:40 -0500

    Edit styling for Business card

--- src/Business/Business.js
+++ src/Business/Business.js
@@ -1,24 +1,17 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import styles from './Business.module.css';
 
 //This component represents how each business restaurant is formatted & styled
 const Business = ({ business }) => {
 
-    const [ text, setText ] = useState("♡");
-
-    const addToFavorites = () => {
-        setText(text === "♡" ? "❤️" : "♡");
-    }
-
     return (
         <div className={styles.Business}>
-            <button className={styles.favoriteButton} onClick={addToFavorites}>{text}</button>
             <div className={styles.imageSection}>
               <img src= {business.imageSrc} alt= '' />
             </div>
             <div className={styles.card}>
-                <h1><Link to="/businesses/id">{business.name}</Link></h1>
+                <h1>{business.name}</h1>
                 <p className={styles.address}>{business.address}</p>
                 <p className={styles.address}>{business.address2}</p>
                 <p>Phone: {business.phone}</p>
