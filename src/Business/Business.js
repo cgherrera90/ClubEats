70d15e7cb4687d333f0c36bc40333fb208740697
Carla Herrera <cgherrera07@gmail.com>
2024-01-23 16:21:37 -0500

    Add functions to selectively display different info based on what is available from API. Edit styling for business list cards

--- src/Business/Business.js
+++ src/Business/Business.js
@@ -4,6 +4,24 @@ import styles from './Business.module.css';
 //This component represents how each business restaurant is formatted & styled using the info returned from Yelp API (determined in Yelp component in utils)
 const Business = ({ business }) => {
 
+    //Function to handle businesses with no phone number listed to prevent empty spaces
+    const phoneNumber = () => {
+        if (business.phone === "") return "N/A";
+        else return business.phone;
+    }
+
+    //Function to handle businesses with no price listed to prevent empty spaces
+    const priceListed = () => {
+        if (!business.price) return "N/A";
+        return business.price;
+    }
+
+    //Function to shorten ice cream category name
+    const category = () => {
+        if (business.category === "Ice Cream & Frozen Yogurt") return "Desserts";
+        else return business.category;
+    }
+
     return (
         <div className={styles.Business}>
             <div className={styles.imageSection}>
@@ -11,13 +29,20 @@ const Business = ({ business }) => {
             </div>
             <div className={styles.card}>
                 <h1>{business.name}</h1>
-                <p className={styles.address}>{business.address}</p>
-                <p className={styles.address}>{business.address2}</p>
-                <p>Phone: {business.phone}</p>
-                <p>Price: {business.price}</p>
-                <p>Category: {business.category}</p>
-                <p>Rating: {business.rating}</p>
-                <p>Review Count: {business.reviewCount}</p>
+
+                <h4 className={styles.address}>{business.address}</h4>
+
+                <h4 className={styles.address}>{business.address2}</h4>
+
+                <p><span className={styles.accent}>Phone: </span>{phoneNumber()}</p>
+
+                <p><span className={styles.accent}>Price: </span> {priceListed()}</p>
+
+                <p><span className={styles.accent}>Category: </span>{category()}</p>
+
+                <p><span className={styles.accent}>Rating: </span>{business.rating}</p>
+
+                <p><span className={styles.accent}>Review Count: </span>{business.reviewCount}</p>
                 </div>
             </div>
     );
